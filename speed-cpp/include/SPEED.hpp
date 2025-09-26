a46833ea51bd0064ce2cdd52732af05cd127b7b6
Anirudh Lakhanpal <anirudhlakhanpal123@gmail.com>
2025-09-26 19:46:39 +0530

    added thread safety to the SPEED API class

--- speed-cpp/include/SPEED.hpp
+++ speed-cpp/include/SPEED.hpp
@@ -8,18 +8,22 @@
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <mutex>
+
 namespace SPEED {
 enum class ThreadMode { Single = 0, Multi = 1 };
-class SPEED {
 
+class SPEED {
 public:
   void sendMessage();
   void kill();
   void stop();
   void resume();
   void start();
+
   SPEED(const std::string &, const ThreadMode &, const std::filesystem::path &);
   SPEED(const std::string &, const ThreadMode &);
+
   bool setKeyFile(const std::filesystem::path &);
   void setCallback(std::function<void(const PMessage &)> cb);
   void trigger();
@@ -29,12 +33,19 @@ private:
   std::string self_proc_name_;
   ThreadMode tmode_;
   std::filesystem::path speed_dir_;
-  std::filesystem::path communication_dir;
+  std::filesystem::path communication_dir_;
+
   std::string key_;
-  std::atomic<long long> seq_number_;
   std::filesystem::path key_path_;
+  std::atomic<long long> seq_number_{0};
+
   std::function<void(const PMessage &)> callback_;
   std::unique_ptr<AccessRegistry> access_list_;
+
+  mutable std::mutex callback_mutex_;
+  mutable std::mutex access_list_mutex_;
+  mutable std::mutex key_mutex_;
+
   void processIncomingMessage_(const Message &);
 };
 } // namespace SPEED
--- speed-cpp/src/SPEED.cpp
+++ speed-cpp/src/SPEED.cpp
@@ -5,9 +5,9 @@ namespace SPEED {
 
 SPEED::SPEED(const std::string &proc_name, const ThreadMode &tmode,
              const std::filesystem::path &speed_dir) {
-  this->self_proc_name_ = proc_name;
-  this->tmode_ = tmode;
-  this->speed_dir_ = speed_dir;
+  self_proc_name_ = proc_name;
+  tmode_ = tmode;
+  speed_dir_ = speed_dir;
 
   if (!Utils::directoryExists(speed_dir_)) {
     std::cout << "[ERROR]: Speed Dir does not exist\n";
@@ -19,7 +19,7 @@ SPEED::SPEED(const std::string &proc_name, const ThreadMode &tmode,
 
   std::cout << "[INFO]: Speed DIR: " << speed_dir_ << "\n";
   if (!Utils::directoryExists(speed_dir_ / "access_registry")) {
-    std::cout << "[ERROR]: Registry dir doesnt exist \n";
+    std::cout << "[ERROR]: Registry dir doesn't exist \n";
     if (Utils::createAccessRegistryDir(speed_dir_ / "access_registry")) {
       std::cout << "[INFO]: Created Registry Dir\n";
     }
@@ -36,21 +36,44 @@ SPEED::SPEED(const std::string &proc_name, const ThreadMode &tmode)
 
 bool SPEED::setKeyFile(const std::filesystem::path &key_path) {
   if (!Utils::fileExists(key_path)) {
-    std::cout << "[ERROR]: Key Path Doen't exist\n";
+    std::cout << "[ERROR]: Key Path doesn't exist\n";
     return false;
   }
+
+  std::lock_guard<std::mutex> lock(key_mutex_);
   key_ = KeyManager::getKeyFromConfigFile(key_path);
-  this->key_path_ = key_path;
+  key_path_ = key_path;
   return true;
 }
+
 void SPEED::setCallback(std::function<void(const PMessage &)> cb) {
+  std::lock_guard<std::mutex> lock(callback_mutex_);
   callback_ = std::move(cb);
 }
-void SPEED::processIncomingMessage_(const Message &message) {
-  // validation and all is handled here.
+
+void SPEED::trigger() {
+  std::function<void(const PMessage &)> cb_copy;
+  {
+    std::lock_guard<std::mutex> lock(callback_mutex_);
+    cb_copy = callback_; // copy the callback to call outside the lock
+  }
+
+  if (cb_copy) {
+    PMessage msg("Lemon", "Hello lol", 69420);
+    cb_copy(msg);
+  }
 }
+
 void SPEED::addProcess(const std::string &proc_name) {
-  access_list_->addProcessToList(proc_name);
+  std::lock_guard<std::mutex> lock(access_list_mutex_);
+  if (access_list_) {
+    access_list_->addProcessToList(proc_name);
+  }
 }
-void SPEED::trigger() {}
+
+void SPEED::processIncomingMessage_(const Message &message) {
+  // validation and handling here
+  // acquire locks if modifying callback_, access_list_, key_, etc.
+}
+
 } // namespace SPEED
